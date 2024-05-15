01a1c122655599f56e655f21d699e14b902fff1b
Arun Kumar <ak3930566@gmail.com>
2024-05-16 00:02:00 +0530

    path of max gold and safest path in grid

--- path_max_gold.cpp
+++ path_max_gold.cpp
@@ -1,7 +1,5 @@
 /*
 Q1219 Path with Maximum Gold
-
-!! NOT OTPIMISE;
 */
 
 #include <bits/stdc++.h>
@@ -9,73 +7,59 @@ using namespace std;
 
 class Solution
 {
-
-  // declaring rows and cols as global var, so they available for every function
-  int rows, cols;
-
 public:
-  int getMaximumGold(vector<vector<int>> &grid)
+  // rows and cols in grid vector;
+  int m, n;
+  vector<vector<int>> directions{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+  int DFS(vector<vector<int>> &grid, int i, int j)
   {
-    rows = grid.size();
-    cols = grid[0].size();
 
-    vector<vector<int>> visited(rows, vector<int>(cols, 0));
-
-    // storing total gold
-    int totalGold = 0;
-    for (int i = 0; i < rows; i++)
+    if (i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == 0)
     {
-      for (int j = 0; j < cols; j++)
-      {
-        totalGold += grid[i][j];
-      }
+      return 0; // Zero gold
     }
+
+    int cellGold = grid[i][j];
+    grid[i][j] = 0;
 
     int maxGold = 0;
 
-    for (int i = 0; i < rows; i++)
+    // up, down, left, right
+    for (vector<int> &dir : directions)
     {
-      for (int j = 0; j < cols; j++)
+      int new_i = i + dir[0];
+      int new_j = j + dir[1];
+
+      maxGold = max(maxGold, DFS(grid, new_i, new_j));
+    }
+
+    grid[i][j] = cellGold;
+    return cellGold + maxGold;
+  }
+
+  int getMaximumGold(vector<vector<int>> &grid)
+  {
+    m = grid.size();
+    n = grid[0].size();
+
+    int maxGold = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
       {
-        maxGold = max(maxGold, dfs(grid, visited, i, j));
-        if (maxGold == totalGold)
+
+        if (grid[i][j] != 0)
         {
-          return maxGold;
+          // It has gold
+          maxGold = max(maxGold, DFS(grid, i, j));
         }
       }
     }
+
     return maxGold;
   }
-
-  int dfs(vector<vector<int>> &grid, vector<vector<int>> &visited, int row, int col)
-  {
-    // check for out of bound , visited and zero gold conditions
-    if (row >= rows || col >= cols || visited[row][col] == 1 || grid[row][col] == 0)
-    {
-      return 0;
-    }
-
-    visited[row][col] = 1;
-
-    int maxGold = 0;
-    vector<vector<int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
-    for (int i = 0; i < 4; i++)
-    {
-      int nextRow = row + dir[i][0];
-      int nextCol = col + dir[i][1];
-      if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols)
-      {
-        maxGold = max(maxGold, dfs(grid, visited, nextRow, nextCol));
-      }
-    }
-
-    visited[row][col] = 0;
-
-    return grid[row][col] + maxGold;
-  }
 };
-
 /*
 Input: grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]]
 Output: 28
